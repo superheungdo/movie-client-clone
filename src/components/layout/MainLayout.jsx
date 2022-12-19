5ea1d69cf1182d415b730c79c5f068f4f8bf34b6
superbeom <it.super.beom@gmail.com>
2022-12-19 16:39:55 +0900

    Create Header

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -3,6 +3,7 @@ import { Box } from "@mui/material";
 
 import GlobalLoading from "../common/GlobalLoading";
 import Footer from "../common/Footer";
+import Topbar from "../common/Topbar";
 
 const MainLayout = () => {
   return (
@@ -10,10 +11,11 @@ const MainLayout = () => {
       {/* Global Loading */}
       <GlobalLoading />
 
-      {/* Login Mmodal */}
+      {/* Login Modal */}
 
       <Box display="flex" minHeight="100vh">
         {/* header */}
+        <Topbar />
 
         {/* main */}
         <Box component="main" flexGrow={1} overflow="hidden" minHeight="100vh">
