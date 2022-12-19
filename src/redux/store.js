2b017c8499376eacf31348bced7e0f4fdc36f0c5
superbeom <it.super.beom@gmail.com>
2022-12-19 14:58:57 +0900

    Update Redux Store > Add AppState, ThemeMode, GlobalLoading, AuthModal

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import appStateSlice from "./features/appStateSlice";
+import themeModeSlice from "./features/themeModeSlice";
+import globalLoadingSlice from "./features/globalLoadingSlice";
+import authModalSlice from "./features/authModalSlice";
 import userSlice from "./features/userSlice";
 
 const store = configureStore({
   reducer: {
+    appState: appStateSlice,
+    themeMode: themeModeSlice,
+    globalLoading: globalLoadingSlice,
+    authModal: authModalSlice,
     user: userSlice,
   },
 });
