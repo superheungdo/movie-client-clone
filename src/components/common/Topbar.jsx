455e9b49916263a41a1d71a77967d775166aa92e
superbeom <it.super.beom@gmail.com>
2022-12-20 10:01:08 +0900

    Update Header > Create Sidebar and UserMenu

--- src/components/common/Topbar.jsx
+++ src/components/common/Topbar.jsx
@@ -21,6 +21,8 @@ import menuConfigs from "../../configs/menu.configs";
 import { themeModes } from "../../configs/theme.configs";
 
 import Logo from "./Logo";
+import UserMenu from "./UserMenu";
+import Sidebar from "./Sidebar";
 
 const ScrollAppBar = ({ children, window }) => {
   const { themeMode } = useSelector((state) => state.themeMode);
@@ -52,6 +54,8 @@ const Topbar = () => {
   const { themeMode } = useSelector((state) => state.themeMode);
   const { user } = useSelector((state) => state.user);
 
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   const dispatch = useDispatch();
 
   const onSwithTheme = () => {
@@ -60,70 +64,84 @@ const Topbar = () => {
     dispatch(setThemeMode(theme));
   };
 
+  const toggleSidebar = () => setSidebarOpen(!sidebarOpen);
+
   return (
-    <ScrollAppBar>
-      <AppBar elevation={0} sx={{ zIndex: 9999 }}>
-        <Toolbar sx={{ alignItems: "center", justifyContent: "space-between" }}>
-          <Stack direction="row" spacing={1} alignItems="center">
-            <IconButton color="inherit" sx={{ mr: 2, display: { md: "none" } }}>
-              <MenuIcon />
-            </IconButton>
+    <>
+      <Sidebar open={sidebarOpen} toggleSidebar={toggleSidebar} />
 
-            <Box sx={{ display: { xs: "inline-block", md: "none" } }}>
-              <Logo />
-            </Box>
-          </Stack>
-
-          {/* main menu */}
-          <Box
-            flexGrow={1}
-            alignItems="center"
-            display={{ xs: "none", md: "flex" }}
+      <ScrollAppBar>
+        <AppBar elevation={0} sx={{ zIndex: 9999 }}>
+          <Toolbar
+            sx={{ alignItems: "center", justifyContent: "space-between" }}
           >
-            <Box sx={{ marginRight: "30px" }}>
-              <Logo />
+            <Stack direction="row" spacing={1} alignItems="center">
+              <IconButton
+                color="inherit"
+                sx={{ mr: 2, display: { md: "none" } }}
+                onClick={toggleSidebar}
+              >
+                <MenuIcon />
+              </IconButton>
+
+              <Box sx={{ display: { xs: "inline-block", md: "none" } }}>
+                <Logo />
+              </Box>
+            </Stack>
+
+            {/* main menu */}
+            <Box
+              flexGrow={1}
+              alignItems="center"
+              display={{ xs: "none", md: "flex" }}
+            >
+              <Box sx={{ marginRight: "30px" }}>
+                <Logo />
+              </Box>
+
+              {menuConfigs.main.map((item, index) => (
+                <Button
+                  key={index}
+                  sx={{
+                    color: appState.includes(item.state)
+                      ? "primary.contrastText"
+                      : "inherit",
+                    mr: 2,
+                  }}
+                  component={Link}
+                  to={item.path}
+                  variant={appState.includes(item.state) ? "contained" : "text"}
+                >
+                  {item.display}
+                </Button>
+              ))}
+
+              <IconButton sx={{ color: "inherit" }} onClick={onSwithTheme}>
+                {themeMode === themeModes.dark ? (
+                  <DarkModeOutlinedIcon />
+                ) : (
+                  <WbSunnyOutlinedIcon />
+                )}
+              </IconButton>
             </Box>
 
-            {menuConfigs.main.map((item, index) => (
-              <Button
-                key={index}
-                sx={{
-                  color: appState.includes(item.state)
-                    ? "primary.contrastText"
-                    : "inherit",
-                  mr: 2,
-                }}
-                component={Link}
-                to={item.path}
-                variant={appState.includes(item.state) ? "contained" : "text"}
-              >
-                {item.display}
-              </Button>
-            ))}
-
-            <IconButton sx={{ color: "inherit" }} onClick={onSwithTheme}>
-              {themeMode === themeModes.dark ? (
-                <DarkModeOutlinedIcon />
-              ) : (
-                <WbSunnyOutlinedIcon />
+            {/* user menu */}
+            <Stack spacing={3} direction="row" alignItems="center">
+              {!user && (
+                <Button
+                  variant="contained"
+                  onClick={() => dispatch(setAuthModalOpen(true))}
+                >
+                  sign in
+                </Button>
               )}
-            </IconButton>
-          </Box>
+            </Stack>
 
-          {/* user menu */}
-          <Stack spacing={3} direction="row" alignItems="center">
-            {!user && (
-              <Button
-                variant="contained"
-                onClick={() => dispatch(setAuthModalOpen(true))}
-              >
-                sign in
-              </Button>
-            )}
-          </Stack>
-        </Toolbar>
-      </AppBar>
-    </ScrollAppBar>
+            {user && <UserMenu />}
+          </Toolbar>
+        </AppBar>
+      </ScrollAppBar>
+    </>
   );
 };
 
