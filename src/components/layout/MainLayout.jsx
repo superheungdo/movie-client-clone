8ab19626bc2c22cb10dab72eb44d374f223a83bd
superbeom <it.super.beom@gmail.com>
2022-12-20 12:07:18 +0900

    Update MainLayout - Get UserInfo and Favorites

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,17 +1,53 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { Box } from "@mui/material";
+import { toast } from "react-toastify";
+
+import { setListFavorites, setUser } from "../../redux/features/userSlice";
+
+import userApi from "../../api/modules/user.api";
+import favoriteApi from "../../api/modules/favorite.api";
 
 import GlobalLoading from "../common/GlobalLoading";
 import Footer from "../common/Footer";
 import Topbar from "../common/Topbar";
+import AuthModal from "../common/AuthModal";
 
 const MainLayout = () => {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    const authUser = async () => {
+      const { response, err } = await userApi.getInfo();
+
+      if (response) dispatch(setUser(response));
+      if (err) dispatch(setUser(null));
+    };
+
+    authUser();
+  }, [dispatch]);
+
+  useEffect(() => {
+    const getFavorites = async () => {
+      const { response, err } = await favoriteApi.getList();
+
+      if (response) dispatch(setListFavorites(response));
+      if (err) toast.error(err.message);
+    };
+
+    if (user) getFavorites();
+    if (!user) dispatch(setListFavorites([]));
+  }, [user, dispatch]);
+
   return (
     <>
       {/* Global Loading */}
       <GlobalLoading />
 
       {/* Login Modal */}
+      <AuthModal />
 
       <Box display="flex" minHeight="100vh">
         {/* header */}
